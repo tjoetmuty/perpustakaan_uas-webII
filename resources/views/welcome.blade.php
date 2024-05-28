89e0dacbf90df63fe890309e3952197b2e584a85
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-05-28 21:53:51 +0800

    styling welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,10 @@
   <div class="hero-overlay bg-opacity-60"></div>
   <div class="hero-content text-center text-neutral-content">
     <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">Welcome to Your Library Buddy!</h1>
-      <p class="mb-5">Let's explore the various book collections in Your Library Buddy and find an enjoyable read for you!.</p>
-      <button class="btn btn-primary" type="submit"><a href="{{route('user.index')}}">Get Started</a></button>
+      <h1 class="mb-5 text-5xl font-bold shadow-3xl">Welcome to Your Library Buddy!</h1>
+      <p class="mb-5 text-white shadow-md bg-slate-700 rounded-xl p-3">Let's explore the various book collections in Your Library Buddy and find an enjoyable read for you!.</p>
+      <button class="btn btn-primary order-2 border-solid border-black rounded-md 
+                    bg-black text-white hover:bg-slate-700 hover:border-slate-700" type="submit"><a href="{{route('user.index')}}">Get Started</a></button>
     </div>
   </div>
 </div>
